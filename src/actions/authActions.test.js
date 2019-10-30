fd6bc6fe9d2f3652ea05f240453ec820f1eca5b4
Jack McConnell <jackmc1122@gmail.com>
2019-10-30 07:39:03 -0700

    Add verify and log out action tests

--- src/actions/authActions.test.js
+++ src/actions/authActions.test.js
@@ -5,12 +5,19 @@ import {
   signUpUser,
   SIGN_UP_LOADING,
   SIGN_UP,
+  VERIFY,
+  VERIFY_LOADING,
+  LOG_OUT_LOADING,
+  LOG_OUT,
+  verifyUser,
+  logOutUser,
 } from '../actions/authActions';
 
 jest.mock('../services/auth-api.js', () => ({
   fetchLogin: () => Promise.resolve({ username: 'jack' }),
   fetchSignUp: () => Promise.resolve({ username: 'jack' }),
-  fetchVerify: () => Promise.resolve({ username: 'jack', balance: 0 })
+  fetchVerify: () => Promise.resolve({ username: 'jack', balance: 0 }),
+  fetchLogOut: () => Promise.resolve({})
 }));
 
 describe('Auth Actions', () => {
@@ -44,6 +51,39 @@ describe('Auth Actions', () => {
         expect(dispatch).toHaveBeenCalledWith({
           type: SIGN_UP,
           payload: { username: 'jack' }
+        });
+      });
+  });
+
+  it('it dispatches a loading action and a fetch log out action', () => {
+    const thunk = logOutUser();
+    const dispatch = jest.fn();
+
+    return thunk(dispatch)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledWith({
+          type: LOG_OUT_LOADING
+        });
+
+        expect(dispatch).toHaveBeenCalledWith({
+          type: LOG_OUT,
+        });
+      });
+  });
+
+  it('it dispatches a loading action and a fetch verify action', () => {
+    const thunk = verifyUser();
+    const dispatch = jest.fn();
+
+    return thunk(dispatch)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledWith({
+          type: VERIFY_LOADING
+        });
+
+        expect(dispatch).toHaveBeenCalledWith({
+          type: VERIFY,
+          payload: { username: 'jack', balance: 0 }
         });
       });
   });
